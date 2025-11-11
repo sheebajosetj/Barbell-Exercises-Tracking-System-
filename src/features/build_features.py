c28705b3d309ab01520e9b69f43432e81e1c7f9e
sheebatjose <sheebatjose@gmail.com>
2025-11-11 21:33:00 +0530

    update

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -11,31 +11,105 @@ from TemporalAbstraction import NumericalAbstraction
 # --------------------------------------------------------------
 df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenets.pkl")
 
+predictor_columns= df.columns[:6]
+
+#Plot settings 
+
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
 # --------------------------------------------------------------
 # Dealing with missing values (imputation)
 # --------------------------------------------------------------
+for col in predictor_columns:
+    df[col].interpolate()
 
+df.info()
 
 # --------------------------------------------------------------
 # Calculating set duration
 # --------------------------------------------------------------
 
+df[df["set"] == 25]["acc_y"].plot()
+df[df["set"] == 50]["acc_y"].plot()
+
+for s in df['set'].unique():
+    start = df[df["set"] ==s].index[0]
+    stop = df[df["set"] ==s].index[-1]
+
+    duration = stop - start
+    df.loc[{df["set"] == s}, "duration"] = duration.seconds
+duration_df = df.groupby(['category'])["duration"].mean()
+
+duration_df.iloc[0]/5
+duration_df.iloc[1]/10
 
 # --------------------------------------------------------------
 # Butterworth lowpass filter
 # --------------------------------------------------------------
 
+df_lowpass = df.copy
+LowPass = LowPassFilter  
+
+fs = 1000 / 200
+cutoff = 0.5
+
+
+df_lowpass = Lowpass.low_pass_filter(df_lowpass, "acc_py", fsm cutoff, order = 5)
+
+subset = df_lowpass[df_lowpass['set'] == 45]
+print(subset["label"[10]])
+
+fig, ax = plt.subplott(nrows = 2, sharex = True, figsize = (20, 10))
+ax[0].plot(subset["acc_y"].reset_index(drop = True), label = "raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop = True), label = "butterworth filter")
+ax[0].legend(loc= "upper_center".bbbox_to_anchor = (0.5, 1.15), fancy_box = True, shadow = True)
+ax[1].legend(loc= "upper_center".bbbox_to_anchor = (0.5, 1.15), fancy_box = True, shadow = True)
+
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order = 5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + ]
 
 # --------------------------------------------------------------
 # Principal component analysis PCA
 # --------------------------------------------------------------
+df_pca = df_lowpass.copy()
+PCA = PrincipalComponentAnalysis()
+
+PCA.determine_pc_explained_variance(df_pca, predictor_columns)
+pc_values = PCA.determine_pc_explained_variance(df_pca, predictor_columns)
+
+plt.figure(figsize=(10, 10))
+plt.plot(range(1, len(predictor_columns)+1), pc_values)
+plt.xlabel('principal component number')
+plt.ylabel("explained variance")
+plt.show()
+
+df_pca = PCA.apply_pca(df_pca, predictor_columns, 3)
+
+subset = df_pca[df_pca["set"] == 35]
+subset[["pca_1", "pca_2", "pca_3"]].plot()
+
+df_pca 
+
 
 
 # --------------------------------------------------------------
 # Sum of squares attributes
 # --------------------------------------------------------------
 
+acc_r = df_squared["acc_x"] ** 2 + df_squared["acc_y"] ** 2 + df_squared["acc_z"] ** 2
+gyr_r = df_squared["gyr_x"] ** 2 + df_squared["gyr_y"] ** 2 + df_squared["gyr_z"] ** 2
 
+df_squared["acc_r"] = np.sqrt(acc_r)
+df_squared["gyr_r"] = np.sqrt(gyr_r)
+
+subset = df_squared[df_squared['set'] == 14]
+subset[["acc_r", gyr_r]].plot(subplots = True)
 # --------------------------------------------------------------
 # Temporal abstraction
 # --------------------------------------------------------------
